9faf28900c3fbc4d1c2244d62cb38cf1009fee66
Leandro Martello <maidenleo2000@gmail.com>
2023-02-09 14:06:13 -0300

    Promesas primera parte

--- src/concepts/03-promesas.js
+++ src/concepts/03-promesas.js
@@ -4,9 +4,39 @@ import { heroes } from "../data/heroes";
  * 
  * @param {HTMLDivElement} element 
  */
-export const promiseComponent = (element) =>{
+export const promiseComponent = (element) => {
     console.log('promiseComponent');
 
+    //renderiza el hero nada mas
+    const renderHero = (hero) => {
+        element.innerHTML = hero.name;
+    }
+
+
+    const renderTwoHeros = (hero1, hero2) =>{
+        element.innerHTML = `
+            <h3>${hero1.name}</h3>
+            <h3>${hero2.name}</h3>
+        `;
+    }
+
+
+    const renderError = (error) => {
+        element.innerHTML = `
+                <h1>Error</h1>
+                <h3>${error}</h3>
+            `;
+    }
+
+    const id1 = '5d86371fd55e2e2a30fe1ccb';
+    const id2 = '5d86371f97c29d020f1e1f6d';
+
+
+    //USAR LA PROMESA
+    findHero(id1)
+        //.then(cualquierNombre => renderHero(cualquierNombre));
+        .then(renderHero) //esto es lo mismo que arriba en el caso que los argumentos de la izquierda los use en el mismo orden que en el pase a funcion de la derecha. Es decir lo que devuelve la promesa lo envia a la funcion.
+        .catch(error => renderError(error));
 
 
 }
@@ -19,16 +49,16 @@ export const promiseComponent = (element) =>{
  */
 const findHero = (id) => {
 
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const hero = heroes.find(hero => hero.id === id);
 
         if (hero) {
-            resolve (hero);
+            resolve(hero);
             return;
-        } 
+        }
         reject(`El heroe con el ID ${id} no se encuentra `);
     });
-    
+
 }
 
 
@@ -40,7 +70,7 @@ const findHero = (id) => {
 //         if (hero) {
 //             resolve (hero);
 //             return;
-//         } 
+//         }
 //         reject(`El heroe con el ID ${id} no se encuentra `);
 //     });
 
